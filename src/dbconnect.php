15759be96b8f9315cebee3997ec9a2ad30ba324c
Sophie Nhan <sophie-nhan@hotmail.com>
2022-12-29 13:33:12 +0100

    Inital commit

---
+++ src/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+    $host       = "localhost";
+    $database   = "BookMarked";
+    $user       = "root";
+    $password       = "root";
+    $charset    = "utf8mb4";
+
+    $dsn = "mysql:host={$host};dbname={$database};charset={$charset}";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+
+    try {
+        $pdo = new PDO($dsn, $user, $password, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+?>
